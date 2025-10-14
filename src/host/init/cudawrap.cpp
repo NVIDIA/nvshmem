5f74c705744b0c856981333d5b2a821e1b549315
Seth Howell <sethh@nvidia.com>
2025-10-13 17:15:09 -0700

    [General] File cleanup for 3.5 RC 2.

--- src/host/init/cudawrap.cpp
+++ src/host/init/cudawrap.cpp
@@ -1,7 +1,7 @@
 /*************************************************************************
  * Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
  *
- * See LICENSE.txt for license information
+ * See License.txt for license information
  ************************************************************************/
 
 #include <cuda.h>  // for CUresult
