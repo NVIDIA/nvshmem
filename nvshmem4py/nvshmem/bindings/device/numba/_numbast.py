bdddb7faa033646615b16568da39bc4b2cc695d9
Seth Howell <sethh@nvidia.com>
2025-10-16 12:39:32 -0700

    [ADMIN] Add generated python bindings to .gitignore. 1760643572

--- nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
+++ nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00ae365393d8cce79a8467b9cb087b84b1e7bf7a0955b27089e31d5802ad146b
+oid sha256:b4a5b3944fdd88661742e667444c2cd82fcc2cddb20409dbfd718b4fd1ddfc4a
 size 5738544
