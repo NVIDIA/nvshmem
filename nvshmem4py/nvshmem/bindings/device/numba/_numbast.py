16bc2eda1a846749a989885ff4c6e82d527b486e
Alex Brooks <albrooks@nvidia.com>
2026-06-30 18:10:28 +0000

    github: update generated artifacts 1782843028

--- nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
+++ nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9845415ccd176d4e9557524e6483b65f30301f62a6233cee88e3a92cf195d104
+oid sha256:0fa462e5d2332be3db93934d98a379d68359e72dbe20093e1fc0ecc4e2b4b86d
 size 3927015
