3f4c6d81225f45f9c42ed9af09bdb3c61eb356d4
Simon Schwitanski <sschwitanski@nvidia.com>
2026-07-17 16:08:15 +0000

    github: update generated artifacts 1784304494

--- nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
+++ nvshmem4py/nvshmem/bindings/device/numba/_numbast.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fa462e5d2332be3db93934d98a379d68359e72dbe20093e1fc0ecc4e2b4b86d
+oid sha256:c481b9ad4e6e6d5202b8fbf96e095faee0e2c121b8f73d986b74cae7843d36e2
 size 3927015
