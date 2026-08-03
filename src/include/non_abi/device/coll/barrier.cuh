220a8a12eef95cccd2071735c5eff5112dab3994
Kishore <kpunniyamurt@nvidia.com>
2026-08-03 07:06:04 -0700

    device: streamline CFT handle synchronization

--- src/include/non_abi/device/coll/barrier.cuh
+++ src/include/non_abi/device/coll/barrier.cuh
@@ -241,6 +241,10 @@ __device__ NVSHMEMI_DEVICE_ALWAYS_INLINE void nvshmemi_barrier_threadgroup(nvshm
     int myIdx = nvshmemi_thread_id_in_threadgroup<SCOPE>();
     nvshmemi_threadgroup_sync<SCOPE>();
     nvshmemi_tma_drain_if_registered();
+#if LE_HW_SW_REQUIREMENTS_MET && defined(NVSHMEM_CFT_HANDLES_SUPPORT)
+    /* Drain any pending fabric operations */
+    nvshmemi_handle_quiet_owned();
+#endif
     if (!nvshmemi_use_ldst_path()) {
         nvshmemi_transfer_quiet<SCOPE>(true, NVSHMEMX_PE_ANY, NULL, NVSHMEMX_QP_ALL);
     } else if (!myIdx) {
