691e1a365f8b3b37cdfebfbabd1839e9f232e688
Simon Schwitanski <sschwitanski@nvidia.com>
2026-08-18 10:33:41 +0000

    Remove `unlikely` from nvshmemi_common_device.cuh

--- src/include/non_abi/device/common/nvshmemi_common_device.cuh
+++ src/include/non_abi/device/common/nvshmemi_common_device.cuh
@@ -1339,7 +1339,7 @@ __device__ NVSHMEMI_DEVICE_ALWAYS_INLINE void nvshmemi_rma_nbi(void *rptr, void 
     } else {
         constexpr uint32_t supported_hints =
             nvshmemi_region_operation_traits<REGION_OPERATION>::supported_hints;
-        if (unlikely((nvshmemi_region_get_block_hints() & supported_hints) != 0)) {
+        if ((nvshmemi_region_get_block_hints() & supported_hints) != 0) {
             nvshmemi_transfer_rma_nbi<SCOPE, CHANNEL_OP, REGION_OPERATION>(rptr, lptr, bytes, pe,
                                                                            qp_index);
         } else {
